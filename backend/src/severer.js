f7eaa021720b637d51b985074d7db5501e5fe131
anand7071 <59311209+anand7071@users.noreply.github.com>
2022-01-23 09:29:43 +0530

    cl
    gh

--- backend/src/severer.js
+++ backend/src/severer.js
@@ -1,23 +1,15 @@
 const express = require("express");
-const path = require("path")
-const hbs = require("hbs")
+
 
 
 const connect = require("./config/mongoDB");
 const itemsControllar = require("./controllar/items.cont")
-const templates_path = path.join(__dirname, "../templates/views")
-const partials_path = path.join(__dirname, "../templates/partials")
+
 
 
 const app = express();
 app.use(express.json());
-app.set("view engine","hbs")
-app.set("views",templates_path)
-hbs.registerPartials(partials_path)
 
-app.get("/",(req,res)=>{
-    res.render("index")
-})
 app.use("/items",itemsControllar);
 
 
